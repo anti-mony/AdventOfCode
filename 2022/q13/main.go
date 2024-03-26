84820f7221cd2727774fe2def234c63648eafd12
Sushant <9670657+anti-mony@users.noreply.github.com>
2024-03-25 23:44:20 -0500

    2022.13.1

--- 2022/q13/main.go
+++ 2022/q13/main.go
@@ -20,18 +20,76 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for _, p := range input {
-		fmt.Println(p)
-	}
-
-	// fmt.Println("> Answer P1: ", solveP1(input))
+	fmt.Println("> Answer P1: ", solveP1(input))
 
 	// fmt.Println("> Answer P2: ", solveP2(input))
+}
+
+func solveP1(pairs []Pair) int {
+	result := 0
+	for i, pair := range pairs {
+		if compare(pair.Left, pair.Right, 1) < 0 {
+			result += i + 1
+			// fmt.Println("# In ORDER #", i+1, "# # #")
+		}
+	}
+
+	return result
+}
+
+func compare(left, right Value, depth int) int {
+	// fmt.Println(makeNLenString(depth), left, right)
+	if left.List == nil {
+		// Left is Singular
+		if right.List == nil {
+			return left.Singular - right.Singular
+		} else {
+			return compare(newListValue(left.Singular), right, depth+1)
+		}
+	} else {
+		// Left is a list
+		if right.List == nil {
+			// Right is Singular
+			return compare(left, newListValue(right.Singular), depth+1)
+		}
+	}
+
+	i := 0
+	for i < len(left.List) && i < len(right.List) {
+		r := compare(left.List[i], right.List[i], depth+1)
+		if r != 0 {
+			return r
+		}
+		i++
+	}
+
+	return len(left.List) - len(right.List)
+}
+
+func makeNLenString(n int) string {
+	r := ""
+	for i := 0; i < n; i++ {
+		r += ">"
+	}
+	return r
 }
 
 type Value struct {
 	List     []Value
 	Singular int
+}
+
+func newValue(singular *int, list []Value) Value {
+	if singular != nil {
+		return Value{Singular: *singular}
+	}
+	return Value{List: list}
+}
+
+func newListValue(v int) Value {
+	return Value{
+		List: []Value{{Singular: v}},
+	}
 }
 
 func (v Value) String() string {
