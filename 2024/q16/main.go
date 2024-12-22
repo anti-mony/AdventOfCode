b68a78c9d4853395347fec53c500f0b31b54d0f6
Sushant Bansal <sushantbansal2@gmail.com>
2024-12-22 01:57:57 -0600

    2024/q16 p2

--- 2024/q16/main.go
+++ 2024/q16/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"math"
 	"os"
+	"slices"
 
 	"advent.of.code/grid"
 	"advent.of.code/list"
@@ -22,40 +23,84 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("A1: ", Q1(maze))
+	steps, paths := Q1(maze)
+
+	fmt.Println("A1: ", steps, paths)
+	// for k, v := range adjList {
+	// 	fmt.Println(k, v)
+	// }
+
+	// fmt.Println("A2: ", Q2(maze, adjList))
 }
 
 type key struct {
 	c grid.Coordinate
 	d grid.Direction
-	n int
 }
 
-func Q1(maze [][]string) int {
-	si, sj := util.FindIndexMatrix(maze, "S")
-	ei, ej := util.FindIndexMatrix(maze, "E")
-	return FindShortestPath(maze, grid.NewCoordinate(si, sj), grid.NewCoordinate(ei, ej))
+type heapKey struct {
+	c    grid.Coordinate
+	d    grid.Direction
+	n    int
+	path []grid.Coordinate
 }
 
-func FindShortestPath(maze [][]string, start, end grid.Coordinate) int {
+func Q1(maze [][]string) (int, int) {
+	s := grid.NewCoordinate(util.FindIndexMatrix(maze, "S"))
+	e := grid.NewCoordinate(util.FindIndexMatrix(maze, "E"))
+	return FindShortestPath(maze, s, e)
+}
+
+func Q2(maze [][]string, adjList map[grid.Coordinate][]grid.Coordinate) int {
+	s := grid.NewCoordinate(util.FindIndexMatrix(maze, "S"))
+	e := grid.NewCoordinate(util.FindIndexMatrix(maze, "E"))
+	fmt.Println(e, s, len(adjList))
+
+	result := 0
+	for i := 0; i < len(maze); i++ {
+		for j := 0; j < len(maze[i]); j++ {
+			if _, found := adjList[grid.NewCoordinate(i, j)]; found {
+				result++
+			}
+		}
+	}
+
+	return result
+}
+
+func FindShortestPath(maze [][]string, start, end grid.Coordinate) (int, int) {
 	seen := map[key]int{}
-
-	hp := list.NewHeap[key](func(a, b key) bool {
+	path := map[grid.Coordinate]bool{}
+	hp := list.NewHeap[heapKey](func(a, b heapKey) bool {
 		return a.n <= b.n
 	})
 
-	hp.Push(key{
-		c: start,
-		d: grid.DirectionEast,
-		n: 0,
+	hp.Push(heapKey{
+		c:    start,
+		d:    grid.DirectionEast,
+		n:    0,
+		path: []grid.Coordinate{start},
 	})
 
 	for hp.Len() > 0 {
 		curr := hp.Pop()
-		seen[key{
+
+		k := key{
 			c: curr.c,
 			d: curr.d,
-		}] = curr.n
+		}
+
+		if _, found := seen[key{}]; found {
+			continue
+		}
+
+		if curr.c == end {
+			for _, v := range curr.path {
+				path[v] = true
+			}
+		}
+
+		seen[k] = curr.n
 
 		for dir := range grid.DIRECTIONS_STRAIGHT {
 			if dir == curr.d.Reverse() {
@@ -63,20 +108,22 @@ func FindShortestPath(maze [][]string, start, end grid.Coordinate) int {
 			} else if dir != curr.d {
 				_, found := seen[key{c: curr.c, d: dir}]
 				if !found {
-					hp.Push(key{
-						c: curr.c,
-						d: dir,
-						n: curr.n + 1000,
+					hp.Push(heapKey{
+						c:    curr.c,
+						d:    dir,
+						n:    curr.n + 1000,
+						path: slices.Clone(curr.path),
 					})
 				}
 			} else {
 				next := curr.c.MoveTowards(curr.d)
 				_, found := seen[key{c: next, d: dir}]
 				if inBound(maze, next.X, next.Y) && !found && maze[next.X][next.Y] != "#" {
-					hp.Push(key{
-						c: next,
-						d: dir,
-						n: curr.n + 1,
+					hp.Push(heapKey{
+						c:    next,
+						d:    dir,
+						n:    curr.n + 1,
+						path: append(slices.Clone(curr.path), next),
 					})
 				}
 			}
@@ -88,7 +135,13 @@ func FindShortestPath(maze [][]string, start, end grid.Coordinate) int {
 		minS = min(minS, seen[key{c: end, d: d}])
 	}
 
-	return minS
+	util.PrintMatrix(maze)
+	for k := range path {
+		maze[k.X][k.Y] = "O"
+	}
+	util.PrintMatrix(maze)
+
+	return minS, len(path)
 }
 
 func inBound(maze [][]string, i, j int) bool {
